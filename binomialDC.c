4b0a54e2c591d19da1a5382d0f1c6d171619d276
Sydney Christenson <sydneychri2025@gmail.com>
2023-11-29 11:00:17 -0600

    Added comments

--- binomialDC.c
+++ binomialDC.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
-// Returns value of Binomial Coefficient C(n, k)
-int binomialCoeff(int n, int k) {
-    // Base cases
-    if (k == 0 || k == n) {
+// Given recurrence in pdf
+int binomialCoeff(int n, int k)
+{
+    if (k == 0 || k == n)
+    {
         return 1;
     }
-
-    // Recursively calculate C(n-1, k-1) + C(n-1, k)
     return binomialCoeff(n - 1, k - 1) + binomialCoeff(n - 1, k);
 }
 
-int main() {
+// added this for consistency
+int main()
+{
     int n, k;
     printf("Please input n: ");
     scanf("%d", &n);
     printf("Please input k: ");
     scanf("%d", &k);
-
     printf("Value of C(%d, %d) is %d\n", n, k, binomialCoeff(n, k));
     return 0;
 }
